49cb326f6a952762d505a3beb5ae81fd14aa0c58
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-05 20:49:19 +0600

    Fourth bonus functionality added

--- scripts/script.js
+++ scripts/script.js
@@ -548,17 +548,37 @@ async function getPostData() {
   // A function for adding article to the read list
   function readList() {
     const getButton = document.getElementsByClassName("readButton");
+    let click = 0;
+      let count = document.getElementById("readCount");
     // console.log(getButton);
     for (let our of getButton) {
       const desiredText =
         our.parentNode.parentNode.childNodes[7].childNodes[1].innerText;
       const viewCount = our.parentNode.childNodes[3].childNodes[3].innerText;
+      
       our.addEventListener("click", function () {
+        click++;
+        count.innerText = click;
         function createReadList() {
           const getList = document.getElementById("readArea");
-          getList.innerHTML = `
-            
-          `;
+          const div = document.createElement("div");
+          div.classList.add("flex");
+          div.classList.add("px-4");
+          div.classList.add("mb-4");
+          div.classList.add("bg-white");
+          div.classList.add("rounded-xl");
+          div.innerHTML = `<p>${desiredText}</p>
+          <div class="flex gap-2 items-center">
+          <svg width="22.500000" height="15.500000" viewBox="0 0 22.5 15.5" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <desc>
+              Created with Pixso.
+          </desc>
+          <defs/>
+          <path id="Vector" d="M9.60009 9.3999C10.0377 9.83752 10.6312 10.0834 11.25 10.0834C11.8688 10.0834 12.4623 9.83752 12.8999 9.3999C13.3375 8.96228 13.5833 8.3689 13.5833 7.75C13.5833 7.1311 13.3375 6.53772 12.8999 6.1001C12.4623 5.66248 11.8688 5.41663 11.25 5.41663C10.6312 5.41663 10.0377 5.66248 9.60009 6.1001C9.1625 6.53772 8.91667 7.1311 8.91667 7.75C8.91667 8.3689 9.1625 8.96228 9.60009 9.3999ZM11.25 14.75C7.05 14.75 3.55 12.4166 0.75 7.75C3.55 3.08337 7.05 0.75 11.25 0.75C15.45 0.75 18.95 3.08337 21.75 7.75C18.95 12.4166 15.45 14.75 11.25 14.75Z" stroke="#12132D" stroke-opacity="0.600000" stroke-width="1.500000" stroke-linejoin="round"/>
+        </svg>
+            ${viewCount}
+          </div>`;
+          getList.appendChild(div);
         }
         createReadList();
       });
@@ -574,7 +594,7 @@ async function getLatestPost() {
     "https://openapi.programming-hero.com/api/retro-forum/latest-posts"
   );
   const data = await res.json();
-  console.log(data[0]);
+  // console.log(data[0]);
 
   // First post data from API
   const firstPost = document.getElementById("firstPost");
@@ -658,7 +678,9 @@ async function getLatestPost() {
       />
     </g>
   </svg>
-  <p class="font-mulish text-[#12132d99] pb-3">${data[0].author?.posted_date || "No Publish Date"}</p>
+  <p class="font-mulish text-[#12132d99] pb-3">${
+    data[0].author?.posted_date || "No Publish Date"
+  }</p>
 </div>
 <p class="font-mulish text-[18px] font-extrabold pb-3">
   ${data[0].title}
@@ -670,7 +692,9 @@ async function getLatestPost() {
   <img src="${data[0].profile_image}" class="w-1/6 rounded-2xl">
   <div>
     <h5 class="font-mulish font-bold">${data[0].author.name}</h5>
-    <p class="font-mulish text-[14px] text-[#12132d99]">${data[0].author?.designation  || "Unknown"}</p>
+    <p class="font-mulish text-[14px] text-[#12132d99]">${
+      data[0].author?.designation || "Unknown"
+    }</p>
   </div>
 </div>
   `;
@@ -757,7 +781,9 @@ async function getLatestPost() {
       />
     </g>
   </svg>
-  <p class="font-mulish text-[#12132d99] pb-3">${data[1].author?.posted_date || "No Publish Date"}</p>
+  <p class="font-mulish text-[#12132d99] pb-3">${
+    data[1].author?.posted_date || "No Publish Date"
+  }</p>
 </div>
 <p class="font-mulish text-[18px] font-extrabold pb-3">
   ${data[1].title}
@@ -769,7 +795,9 @@ async function getLatestPost() {
   <img src="${data[1].profile_image}" class="w-1/6 rounded-2xl">
   <div>
     <h5 class="font-mulish font-bold">${data[1].author.name}</h5>
-    <p class="font-mulish text-[14px] text-[#12132d99]">${data[1].author?.designation  || "Unknown"}</p>
+    <p class="font-mulish text-[14px] text-[#12132d99]">${
+      data[1].author?.designation || "Unknown"
+    }</p>
   </div>
 </div>
   `;
@@ -857,7 +885,9 @@ async function getLatestPost() {
       />
     </g>
   </svg>
-  <p class="font-mulish text-[#12132d99] pb-3">${data[2].author?.posted_date || "No Publish Date"}</p>
+  <p class="font-mulish text-[#12132d99] pb-3">${
+    data[2].author?.posted_date || "No Publish Date"
+  }</p>
 </div>
 <p class="font-mulish text-[18px] font-extrabold pb-3">
   ${data[2].title}
@@ -869,13 +899,12 @@ async function getLatestPost() {
   <img src="${data[2].profile_image}" class="w-1/6 rounded-2xl">
   <div>
     <h5 class="font-mulish font-bold">${data[2].author.name}</h5>
-    <p class="font-mulish text-[14px] text-[#12132d99]">${data[2].author?.designation  || "Unknown"}</p>
+    <p class="font-mulish text-[14px] text-[#12132d99]">${
+      data[2].author?.designation || "Unknown"
+    }</p>
   </div>
 </div>
   `;
-  if (designation !== "string") {
-    return (designation.innerText = "unknown");
-  }
 }
 
 getLatestPost();
